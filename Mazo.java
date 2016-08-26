edc6c2f593fcad74eee0c210e1e6051f4928d623
Bruno Schalch <brunoschalch@gmail.com>
2016-08-26 09:18:35 -0500

    Actualizar Mazo

--- Mazo.java
+++ Mazo.java
@@ -1,4 +1,24 @@
 public class Mazo {
+  
+  private Carta[] cartas= new Carta[54];
+  
+  public Mazo() {
+    generarMazo();
+  }
+  
+  private void generarMazo() {
+    //generar carta por carta y llenar arreglo
+  }
+  
+  public Carta regresarCartaenPosicion(int index) {
+    if (index>53 || index<0) {
+      //nope
+      return cartas[0];
+    } else {
+      return cartas[index];
+    }
+  
+  }
 
 
 
