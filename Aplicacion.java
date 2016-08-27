68132f7c093ad838efe911708d1bd6a9db376bc2
Bruno Schalch <brunoschalch@gmail.com>
2016-08-27 15:24:22 -0500

    Main

--- Aplicacion.java
+++ Aplicacion.java
@@ -1,7 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class Aplicacion {
   
   public static void main(String[] args) {
-  
+    System.out.println("Generando mazo...");
+    Mazo mazo = new Mazo();
+    System.out.println(mazo);
   }
 
 }
